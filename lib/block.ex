5cdf3d2ff01cc8c226a7a5a40a05461471ada5a8
Alex Bradley <alex.robert.bradley@gmail.com>
2018-06-21 12:52:09 -0400

    created block

--- lib/block.ex
+++ lib/block.ex
@@ -1,3 +1,19 @@
 defmodule Block do
+  defstruct [:data, :timestamp, :previous_hash, :hash]
 
+  def new(data, previous_hash) do
+    %Block{
+      data: data,
+      previous_hash: previous_hash,
+      timestamp: DateTime.utc_now()
+    }
+  end
+
+  def initial do
+    %Block{
+      data: "ZERO_DATA",
+      previous_hash: "ZERO_HASH",
+      timestamp: DateTime.utc_now()
+    }
+  end
 end
---
+++ test/block_test.exs
@@ -0,0 +1,8 @@
+defmodule BlockTest do
+  use ExUnit.Case
+  doctest Block
+
+  test "creates an intial block" do
+    assert Block.initial.data, "ZERO_DATA"
+  end
+end
